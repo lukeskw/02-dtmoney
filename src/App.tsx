49dc758f68db322f1abe390e0ae8fb14614f7858
Porfirio Dev <lucasporfirioa@gmail.com>
2021-05-10 22:40:27 -0300

    modulo 1 terminado, projeto configurado e pronto para iniciar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import styled from 'styled-components';
 
-function App() {
+import { GlobalStyle } from "./styles/global";
+
+// const Title = styled.h1`
+//   color: #8257e6;
+//   font-size: 64px;
+// `
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Hello World!!</h1>
+      {/* <Title>Hello World</Title> */}
+      <GlobalStyle />
     </div>
   );
 }
-
-export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #f0f2f5;
+
+        --red: #E52E40;
+        
+        --blue: #5429CC;
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+
+        --shape: #FFF;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    //font-size: 16px (padrão ideal para DESKTOP)
+    html {
+        @media (max-width: 1080px){
+            font-size: 93.75%; //15px
+        }
+        @media (max-width: 720px){
+            font-size: 87.5%; //14px
+        }
+        // 1 rem = 1x o font size, se FS = 16px, 1REM = 16px...
+        //usar o % melhora e muito a ACESSIBILIDADE
+    }
+    body {
+        background: var(---background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        //por padrão o textarea, o input e o button não importam a fonte do body.
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1,h2,h3,h4,h5,h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disable] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+`
